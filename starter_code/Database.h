840bcdd9637522acbf3e02a40187b8750e575a16
HuayuXie <147600538+HuayuXie@users.noreply.github.com>
2023-11-25 16:21:32 +0800

    Update Database.h

--- starter_code/Database.h
+++ starter_code/Database.h
@@ -5,26 +5,38 @@
 #include "Employee.h"
 
 namespace Records {
-	const int kFirstEmployeeNumber = 1000;
+    const int kFirstEmployeeNumber = 1000;
 
-	class Database
-	{
-	public:
-		Employee& addEmployee(const std::string& firstName,
-							  const std::string& lastName);
-		Employee& getEmployee(int employeeNumber);
-		Employee& getEmployee(const std::string& firstName,
-							  const std::string& lastName);
+    class Database
+    {
+    public:
+        // Add an employee to the database
+        Employee& addEmployee(const std::string& firstName,
+                              const std::string& lastName, const std::string& middleName, const std::string& Address);
+        
+        // Get an employee by employee number
+        Employee& getEmployee(int employeeNumber);
+        
+        // Get an employee by first name and last name
+        Employee& getEmployee(const std::string& firstName,
+                              const std::string& lastName);
 
-		void displayAll() const;
-		void displayCurrent() const;
-		void displayFormer() const;
-// Generate a new database with example data
+        // Display all employees
+        void displayAll() const;
+        
+        // Display currently employed employees
+        void displayCurrent() const;
+        
+        // Display former employees (not currently employed)
+        void displayFormer() const;
+        
+        // Generate a new database with example data
         void generateNewDatabase();
         
         // Generate a unique address
         std::string generateUniqueAddress();
- // Save the database to a file
+
+        // Save the database to a file
         void saveToFile(const std::string& fileName);
         
         // Load the database from a file
@@ -35,7 +47,8 @@ namespace Records {
         
         // Search employees by first name
         void searchEmployeesByName();
- // Search employees by middle name
+        
+        // Search employees by middle name
         void searchEmployeesByMiddleName();
         
         // Search employees by last name
@@ -50,9 +63,13 @@ namespace Records {
 		Employee* authenticate(const std::string& loginId, const std::string& password);
 
         void logDebug(const std::string& message);
+        
+    private:
 
-	private:
-		std::vector<Employee> mEmployees;
-		int mNextEmployeeNumber = kFirstEmployeeNumber;
-	};
+        bool gDebugEnabled = false;  // Global debug flag
+
+        std::vector<Employee> mEmployees;
+        
+        int mNextEmployeeNumber = kFirstEmployeeNumber;
+    };
 }
